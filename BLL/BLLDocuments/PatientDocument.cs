0444862daaacc0df8d9dd84387c08af50e5690e8
DESKTOP-LBHS619\Buddhima <kgbuddhima@gmail.com>
2018-01-25 18:37:10 +0530

    BLL: PatientDocument and IPatientDocument added

---
+++ BLL/BLLDocuments/PatientDocument.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.BLLInterfaces;
+using BusinessEntity;
+
+namespace BLL.BLLDocuments
+{
+    public class PatientDocument : IPatientDocument
+    {
+        /// <summary>
+        /// Deactivate patient
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public bool DeletePatient(int patientId)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Get patient by PatientId or PIn
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Patient GetPatient(string value)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get patient by PatientId
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public Patient GetPatientById(int patientId)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get patient by PIN
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <returns></returns>
+        public Patient GetPatientByPin(string pin)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get Patient Collection
+        /// </summary>
+        /// <returns></returns>
+        public List<Patient> GetPatientCollection()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Insert/Update patinet and return full atient objact
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public Patient SavePatient(Patient patient)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+    }
+}
---
+++ BLL/BLLInterfaces/IPatientDocument.cs
@@ -0,0 +1,53 @@
+﻿using BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BLLInterfaces
+{
+    public interface IPatientDocument
+    {
+        /// <summary>
+        /// Deactivate patient
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        bool DeletePatient(int patientId);
+
+        /// <summary>
+        /// Get patient by PatientId or PIn
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        Patient GetPatient(string value);
+
+        /// <summary>
+        /// Get patient by PatientId
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        Patient GetPatientById(int patientId);
+
+        /// <summary>
+        /// Get patient by PIN
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <returns></returns>
+        Patient GetPatientByPin(string pin);
+
+        /// <summary>
+        /// Get Patient Collection
+        /// </summary>
+        /// <returns></returns>
+        List<Patient> GetPatientCollection();
+
+        /// <summary>
+        /// Insert/Update patinet and return full atient objact
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        Patient SavePatient(Patient patient);
+    }
+}
